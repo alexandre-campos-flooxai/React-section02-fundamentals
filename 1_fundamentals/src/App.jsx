149b0edadbbec20a1ff779d67a12bedf1f071152
AlexandreCampos <alexandre.campos.flooxai@gmail.com>
2025-04-16 11:38:21 -0300

    feat:creating cards

--- 1_fundamentals/src/App.jsx
+++ 1_fundamentals/src/App.jsx
@@ -1,21 +1,37 @@
 import React from "react";
+import "./App.css";
 
+import Card from "./components/layout/Card";
 import Aleatorio from "./components/basicos/Aleatorio";
 import Fragmento from "./components/basicos/Fragmento";
 import ComParametro from "./components/basicos/ComParametro";
-import Primeiro from "./components/basicos/Primeiro"
+import Primeiro from "./components/basicos/Primeiro";
 
 export default () => {
-    return(
-        <div id="app">
-            <h1>Fundamentos React</h1>
-            <Aleatorio min={1} max={60}/>
-            <Fragmento/>
-            <ComParametro 
-                title="Situacao do aluno" 
-                aluno="Pedro Silva" 
-                nota={9.3} />
-            <Primeiro />
-        </div>)
-    
+  return (
+    <div className="App">
+      <h1>Fundamentos React</h1>
+      <div className="Cards">
+        <Card titulo="#04 - Desafio aleatorio" color="#FA6900">
+          <Aleatorio min={1} max={60} />{" "}
+        </Card>
+        <Card titulo="#03 - Fragmento" color="#E8B71A">
+          <Fragmento />
+        </Card>
+
+        <Card titulo="#02 - Com Parametro" color="#E94C6f">
+          {" "}
+          <ComParametro
+            title="Situacao do aluno"
+            aluno="Pedro Silva"
+            nota={9.3}
+          />
+        </Card>
+
+        <Card titulo="#01 - Primeiro componente" color="#588C73">
+          <Primeiro />
+        </Card>
+      </div>{" "}
+    </div>
+  );
 };
--- 1_fundamentals/src/components/basicos/Aleatorio.jsx
+++ 1_fundamentals/src/components/basicos/Aleatorio.jsx
@@ -1,22 +1,31 @@
-import React from "react"
+import React from "react";
 
-export default (props) =>{
-const [a, b] = [1, 2];
-const {c, d} = {c:12, d:45};
-console.log(c,d)
+export default (props) => {
+  const [a, b] = [1, 2];
+  const { c, d } = { c: 12, d: 45 };
+  console.log(c, d);
 
-    // const min = props.min ;
-    // const max = props.max ;
+  // const min = props.min ;
+  // const max = props.max ;
 
-    const {min,max} = props;
-    const aleatorio = parseInt(Math.random() * (max - min) + min);
+  const { min, max } = props;
+  const aleatorio = parseInt(Math.random() * (max - min) + min);
 
-    return (
-        <div>
-            <h2>Valor aleatorio</h2>
-            <p><strong>Valor minimo:</strong>{min}</p>
-            <p><strong>Valor maximo:</strong>{max}</p>
-            <p><strong>Valor escolhido:</strong>{aleatorio}</p>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Valor aleatorio</h2>
+      <p>
+        <strong>Valor minimo:</strong>
+        {min}
+      </p>
+      <p>
+        <strong>Valor maximo:</strong>
+        {max}
+      </p>
+      <p>
+        <strong>Valor escolhido:</strong>
+        {aleatorio}
+      </p>
+    </div>
+  );
+};
